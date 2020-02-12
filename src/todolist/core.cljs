7932e501e53b1bd53d03dac2474b7ae66f1df968
Finn Johnsen <johnsenf@gmail.com>
2020-02-12 23:16:49 +0100

    liste todos

--- src/todolist/core.cljs
+++ src/todolist/core.cljs
@@ -4,15 +4,16 @@
 ;;(enable-console-print!)
 ;;(println "This text is printed from src/todolist/core.cljs. Go ahead and edit it and see reloading in action.")
 
-(defonce todoliste (new r/atom))
+(defonce todoliste (r/atom nil))
+
 (defn visliste [todos]
-  [:ol
-   (for [element todos]
-     [:li element
-      ])])
+  (prn todos)
+  [:div "foo"
+    (map (fn [val] [:div val] ) todos)
+   ])
 
 (defn leggtilkontroller [todoliste]
-  (let [tekst (r/atom "")]
+  #_(let [tekst (r/atom "")]
   [:div
    [:input
     {:type "text"
@@ -33,6 +34,7 @@
 
 
 (defn app []
+  (reset! todoliste ["Spise mat" "Rydde kjøkkenet"])
   [:div
    [visliste @todoliste]
    [leggtilkontroller @todoliste]
