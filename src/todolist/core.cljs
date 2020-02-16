1bb43a0017768151ea65c9ae1c2990ee365a51c7
Finn Johnsen <johnsenf@gmail.com>
2020-02-16 20:26:45 +0100

    blank feltet etter :add-todo-item

--- src/todolist/core.cljs
+++ src/todolist/core.cljs
@@ -5,52 +5,37 @@
 
 (enable-console-print!)
 
-
 (rf/reg-event-db :initialize
-  (fn [_ _]
-    {:todos ["Bajse" "Spise mat"]
-            :derp {}}))
+                 (fn [_ _]
+                   {:todos ["Bajse" "Spise mat"]
+                    :derp  {}}))
 
-(rf/reg-event-db
-  :add-todo-item
+(rf/reg-event-db :add-todo-item
   (fn [db [_ new-todo-item]]
     (prn "skal legge til " new-todo-item)
     (assoc db :todos (conj (:todos db) new-todo-item))))
 
-(rf/reg-sub
-  :todo-items
+(rf/reg-sub :todo-items
   (fn [db _]
     (:todos db)))
-
-
 
 (defn visliste [todos]
   (prn todos)
   [:div
-   (map (fn [val] ^{:key val}[:div val]) todos)
-   ])
+   (map (fn [val] ^{:key val} [:div val]) todos)])
 
 (defn leggtilkontroller []
   [:div
-   [:input {:type "text" :on-blur #(rf/dispatch [:add-todo-item (-> % .-target .-value)])
-
+   [:input {:type "text"
             :on-key-press (fn [e]
-                           (if (= 13 (.-charCode e))
-                             (do
-                               (rf/dispatch [:add-todo-item (-> e .-target .-value)])
-                               ;                               (set! (.-value e ) "")
-
-                               )))
-            }]
-   ]
-
-
-  )
-
+                            (if (= 13 (.-charCode e))
+                              (do
+                                (rf/dispatch [:add-todo-item (-> e .-target .-value)])
+                                (set! (-> e .-target .-value) ""))))}]])
 
 (defn app []
   [:div
-   [visliste    @(rf/subscribe  [:todo-items])]
+   [visliste @(rf/subscribe [:todo-items])]
    [leggtilkontroller]])
 
 (r/render [app]
